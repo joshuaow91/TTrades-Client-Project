7d8bf7c1f02be5eccc8efe10054db16256566459
Joshua Ow <joshuaow@gmail.com>
2023-04-18 18:29:44 -0500

    affiliate card, working on copy clipboard

--- client/src/components/Affiliates.jsx
+++ client/src/components/Affiliates.jsx
@@ -23,22 +23,22 @@ const Affiliates = () => {
         </div>
       </div>
 
-      <div className='flex w-full mb-4 bg-zinc-600 p-4 rounded-md items-center'>
+      <a href='https://elitetraderfunding.com/products/?ref=ttrades' target='_blank' className='flex w-full justify-between mb-4 bg-zinc-600 hover:opacity-80 p-4 rounded-md items-center'>
 
-        <div className="flex flex-col  items-center px-4  w-1/3 sm:w-1/4">
+        <div className="flex flex-col items-center px-4  w-1/4 sm:w-1/4">
           <FontAwesomeIcon
             icon={faUser}
             className="w-10 h-10 bg-zinc-700 rounded-full p-5 mb-2 text-zinc-300"
           />
         </div>
 
-        <div className='w-2/3 sm:w-full  text-zinc-400 text-sm sm:text-base flex-col flex'>
+        <div className='w-2/3 sm:w-full  text-zinc-400 text-sm sm:text-base flex-col flex gap-y-2'>
           <h3 className="text-md  text-zinc-300 font-semibold">Elite Trader Funding</h3>
             <span className='inline-flex'>ETF provides traders with an opportunity to earn live funding based on their trading performance in simulated accounts. </span>
             <span className='font-semibold text-white'>CODE: ttradesETF</span>
         </div>
 
-      </div>
+      </a>
       
     
     </div>
