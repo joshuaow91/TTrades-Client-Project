5dd4f4c38f01bd4b934266fb84a910021fb1a4e0
Joshua Ow <joshuaow@gmail.com>
2023-04-20 15:41:28 -0500

    disclosures added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,26 +1,33 @@
 import React, { useState } from "react";
-import Modal from "./Modal";
 
 const Footer = () => {
-    const [showModal, setShowModal] = useState(false);
-
-    const handleShow = () => setShowModal(true);
-    const handleClose = () => setShowModal(false);
 
     return (
-        <div className="flex flex-col sm:flex-row justify-between items-center m-2 text-white text-sm font-light gap-y-1 bg-zinc-800 rounded-md rounded-t-none p-2 sm:bg-transparent">
-            <p className="text-center text-xs">
-                &copy; TTrades 2023. Not Financial Advice.  Made by <a className=" hover:text-white underline" href="https://joshuaow.com">Joshua Ow</a>
-            </p>
-            <button
-                className="bg-sky-800 text-white px-4 py-2 rounded hover:bg-sky-900"
-                onClick={handleShow}
-            >
-                Disclaimer
-            </button>
-            <Modal show={showModal} handleClose={handleClose} />
+        <div className="flex flex-col mt-4 m-2 text-white text-sm font-light gap-x-10 bg-zinc-900 bg-opacity-30 rounded-md p-2 max-w-5xl">
+            <div className="flex flex-col sm:flex-row items-center sm:justify-between mb-3 bg-zinc-800 p-2 rounded">
+              <p className="text-left text-sm">
+                &copy; TTrades 2023. All Rights Reserved.
+              </p>
+              <p>Made by <a className=" hover:text-white underline" href="https://joshuaow.com">Joshua Ow</a></p>
+            </div>
+            <div className="text-xs text-left max-h-44 overflow-scroll p-2 sm:max-h-96">
+                <p className="font-bold">WEBSITE DISCLAIMER</p>
+                <p>
+                    The information provided by TTrades LLC ("we," "us," or "our") on https://ttrades.netlify.app (the "Site") is for general informational purposes only. All information on the Site is provided in good faith, however we make no representation or warranty of any kind, express or implied, regarding the accuracy, adequacy, validity, reliability, availability, or completeness of any information on the Site. UNDER NO CIRCUMSTANCE SHALL WE HAVE ANY LIABILITY TO YOU FOR ANY LOSS OR DAMAGE OF ANY KIND INCURRED AS A RESULT OF THE USE OF THE SITE OR RELIANCE ON ANY INFORMATION PROVIDED ON THE SITE. YOUR USE OF THE SITE AND YOUR RELIANCE ON ANY INFORMATION ON THE SITE IS SOLELY AT YOUR OWN RISK.
+                </p>
+                <p className="font-bold">PROFESSIONAL DISCLAIMER</p>
+                <p>
+                    The Site cannot and does not contain financial advice. The financial information is provided for general informational and educational purposes only and is not a substitute for professional advice. Accordingly, before taking any actions based upon such information, we encourage you to consult with the appropriate professionals. We do not provide any kind of financial advice. THE USE OR RELIANCE OF ANY INFORMATION CONTAINED ON THE SITE IS SOLELY AT YOUR OWN RISK.
+                </p>
+                <p className="font-bold">AFFILIATES DISCLAIMER</p>
+                <p>
+                    The Site may contain links to affiliate websites, and we receive an affiliate commission for any purchases made by you on the affiliate website using such links.
+                </p>
+            </div>
         </div>
     )
 }
 
 export default Footer;
+
+
