ba91b688c4809182aeadff4317d18ad791a485d2
Joshua Ow <joshuaow@gmail.com>
2023-04-20 10:12:06 -0500

    logo size on desktop

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     return (
         <div className="max-w-5xl flex rounded-md m-2">
         <div className="p-2 flex flex-col sm:flex-row justify-evenly items-center rounded-md bg-gradient-to-b from-zinc-800 to zinc-700 border-gray-700 ">
-            <div className="w-1/2 sm:w-1/3">
+            <div className="w-1/2 sm:w-1/4">
                 <a href="/">
                 <img className="sm:w-full w-3/4 my-2 mx-auto bg-gradient-to-br from-zinc-600 to-zinc-700 rounded-full" src={Logo} alt="TTrades Logo"/>
                 </a>
