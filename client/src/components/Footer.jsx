ad98d68a7266ff4b732f2b79521fa00edc469a4f
Joshua Ow <joshuaow@gmail.com>
2023-04-20 10:20:46 -0500

    footer complete

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,9 +10,8 @@ const Footer = () => {
     return (
         <div className="flex flex-col sm:flex-row justify-between items-center m-2 text-white text-sm font-light gap-y-1 bg-zinc-800 rounded-md rounded-t-none p-2 sm:bg-transparent">
             <p className="text-center text-xs">
-                &copy; TTrades 2023. Not Financial Advice. 
+                &copy; TTrades 2023. Not Financial Advice.  Made by <a className=" hover:text-white underline" href="https://joshuaow.com">Joshua Ow</a>
             </p>
-            <span className="text-xs"> Made by <a className=" hover:text-white underline" href="https://joshuaow.com">Joshua Ow</a></span>
             <button
                 className="bg-sky-800 text-white px-4 py-2 rounded hover:bg-sky-900"
                 onClick={handleShow}
